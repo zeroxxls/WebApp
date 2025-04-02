5c96bd93c6f52d5a168964036e8de83a00450a9f
zeroxxl <zeroxxhls@gmail.com>
2025-04-02 23:43:23 +0200

    first page v1.1

--- src/pages/FirstPage/FirstPage.jsx
+++ src/pages/FirstPage/FirstPage.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { HeroSection } from "../../components/FirstPageInfo/components/HeroSection";
+import { TextSection } from "../../components/FirstPageInfo/components/TextSection";
 
-export const FirstPage =()=>{
-    return(
-        <div>
-
-        </div>
-    )
-}
+export const FirstPage = () => {
+    return (
+      <div >
+      <HeroSection/>
+      <TextSection/>
+      </div>
+    );
+};
