e9668ec974f431a27c923913f7449e682977486a
zeroxxls <zeroxxhls@gmail.com>
2025-04-02 21:44:55 +0200

    v1.1

--- src/components/Header/ui/Input.jsx
+++ src/components/Header/ui/Input.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Input =()=>{
     return(
-        <div className="mt-7.5 w-200 max-w-md mx-5 ">
+        <div className="mt-7.5 w-200 max-w-md mx-5  ">
         <input 
           type="text" 
           placeholder="Search" 
-          className="p-2 w-full bg-transparent transition-all duration-300 
-           border-0 border-b-2 border-transparent
+          className="p-2 w-full bg-transparent  transition-all duration-400 
+           border-b-1 border-b-gray-400
            focus:border-b-blue-500 focus:translate-y-[-2px]
            focus:shadow-md focus:outline-none"
         />
--- src/components/Header/ui/Logo.jsx
+++ src/components/Header/ui/Logo.jsx
@@ -5,7 +5,7 @@ export const Logo=()=>{
     return(
     <div className="flex items-center">
         <img src={logo} alt="logo" className="w-25 cursor-pointer"/>
-        <h1 className="text-4xl font-bold bg-gradient-to-r from-blue-600  via-cyan-500 to-cyan-300
+        <h1 className="mx-5 text-4xl font-bold bg-gradient-to-r from-blue-600  via-cyan-500 to-cyan-300
          bg-clip-text text-transparent bg-size-200 animate-gradient
          duration-300 cursor-pointer">Sellica</h1>
     </div>
