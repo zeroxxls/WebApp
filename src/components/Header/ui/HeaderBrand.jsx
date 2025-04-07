0f4cce09a52733a1ecd0d45e24547e420745f069
zeroxxls <zeroxxhls@gmail.com>
2025-04-07 14:59:04 +0200

    rename

--- src/components/Header/ui/HeaderBrand.jsx
+++ src/components/Header/ui/HeaderBrand.jsx
@@ -17,7 +17,7 @@ export const HeaderBrand = ({
       <div className="flex items-center">
         <h1 className={`${textSizes[textSize]} mx-5 font-bold bg-gradient-to-r from-blue-600 via-cyan-500 to-cyan-300 
           bg-clip-text text-transparent bg-size-200 animate-gradient duration-300 cursor-pointer ${className}`}>
-          Sellica
+            Luminio
         </h1>
       </div>
     );
