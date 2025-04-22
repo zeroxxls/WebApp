76bd8ca72699bf4d9ed1f2ac76667a6133e3edef
zeroxxls <zeroxxhls@gmail.com>
2025-04-22 22:19:20 +0200

    fix

--- src/modules/auth/components/LoginSection.jsx
+++ src/modules/auth/components/LoginSection.jsx
@@ -6,7 +6,7 @@ export const LoginSection = () => {
   return (
     <div className="flex items-center justify-center min-h-screen">
         <div className="w-full max-w-sm [background:linear-gradient(45deg,#172033,theme(colors.slate.800)_50%,#172033)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.slate.600/.48)_80%,_theme(colors.indigo.500)_86%,_theme(colors.indigo.300)_90%,_theme(colors.indigo.500)_94%,_theme(colors.slate.600/.48))_border-box] rounded-lg border border-transparent animate-border">
-            <div className="p-8 bg-blue-950/10 border border-blue-900 rounded-lg relative z-10">
+            <div className="p-8 bg-gray-950/25 border border-blue-900 rounded-lg relative z-10">
                 <h2 className="text-white text-xl font-bold text-center mb-4">Sign In</h2>
                 <AuthInput placeholder={'Name'}/>
                 <AuthInput placeholder={'Password'} type={'password'}/>
