e34817f8bcfb1f43bd61cb144500bc2f7ce4154a
zeroxxls <zeroxxhls@gmail.com>
2025-05-02 13:00:10 +0200

    action btns

--- src/modules/MainContent/components/ModalWindow/ModalWindow.jsx
+++ src/modules/MainContent/components/ModalWindow/ModalWindow.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { useState ,useEffect} from 'react'
 import { ActionBtn } from '../../ui/ActionBtn'
+import { AiOutlineLike, AiFillLike } from 'react-icons/ai';
+import { FiBookmark } from 'react-icons/fi';
+import { FaBookmark } from "react-icons/fa";
 import '../../../../shared/styles/hideScrollBar.css'
 
 export const ModalWindow = ({ onClose, selectedWork, selectedUser }) => {
+  const likeKey = `post_${selectedWork.id}_liked`
+  const saveKey = `post_${selectedWork.id}_saved`;
+  const [isLiked, setIsLiked] = useState(
+    localStorage.getItem(likeKey)=== 'true'
+  )
+  const [isSaved, setIsSaved] = useState(
+    localStorage.getItem(saveKey) === 'true'
+  )
+  useEffect(()=>{
+    localStorage.setItem(likeKey, isLiked)
+    localStorage.setItem(saveKey, isSaved)
+  },[isLiked, isSaved])
+  const handleLike=()=>{
+    setIsLiked(!isLiked)
+  }
+  const handleSave=()=>{
+    setIsSaved(!isSaved)
+    }
+
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center p-4">
       {/* Оверлей */}
@@ -62,10 +85,20 @@ export const ModalWindow = ({ onClose, selectedWork, selectedUser }) => {
             </div>
           )}
 
-          <div>
+          <div className='m-2 mb-8'>
             <div className="flex space-x-4">
-              <ActionBtn variant="like" >Like</ActionBtn>
-              <ActionBtn variant="save">Save</ActionBtn>
+              <ActionBtn
+               variant="like"
+               isActive={isLiked}
+               onClick={handleLike}
+               >{isLiked ? <AiFillLike className="w-6 h-6"/> : <AiOutlineLike className="w-6 h-6" />} Like
+               </ActionBtn>
+
+              <ActionBtn 
+              variant="save"
+              isActive={isSaved}
+              onClick={handleSave}
+              >{isSaved? <FaBookmark className="w-6 h-6"/> : <FiBookmark className="w-6 h-6"/>}Save</ActionBtn>
             </div>
           </div>
 
--- src/modules/MainContent/ui/ActionBtn.jsx
+++ src/modules/MainContent/ui/ActionBtn.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
-import clsx from "clsx";
+import React from 'react';
+import clsx from 'clsx';
 
-export const ActionBtn = ({children, variant='like', className='', ...props}) => {
-    const baseClasses = "bg-blue-600 hover:bg-blue-500 text-white rounded-lg px-4 py-2  transition duration-300"
-    const variants={
-        like:"bg-blue-600 hover:bg-blue-500",
-        save:"bg-gray-600/70 hover:bg-gray-600",
-    };
+export const ActionBtn = ({
+  children,
+  variant = 'like',
+  isActive = false,
+  className = '',
+  ...props
+}) => {
+  const baseClasses = `
+    flex items-center justify-center gap-2
+    text-white rounded-lg px-4 py-2
+    transition-all duration-300
+    cursor-pointer text-xl w-xl
+    hover:scale-101
+  `;
+
+  const variants = {
+    like: isActive ? 'bg-blue-500 shadow-md' : 'bg-blue-600',
+    save: isActive ? 'bg-gray-600 shadow-md' : 'bg-gray-600/70',
+  };
+
   return (
-    <button className={clsx(baseClasses, variants[variant], className)} {...props}>
-        {children}
+    <button
+      className={clsx(baseClasses, variants[variant], className)}
+      aria-pressed={isActive}
+      {...props}
+    >
+      {children}
     </button>
-  )
-}
+  );
+};
