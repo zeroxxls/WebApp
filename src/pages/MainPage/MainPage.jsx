e6f28837ebef375ddba7f0eed9a741d8d3aaeeaa
zeroxxls <zeroxxhls@gmail.com>
2025-05-05 14:18:13 +0200

    fix 1.4.1

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -9,7 +9,7 @@ const MainPage = () => {
         <FilterProvider>
             <div>
                 <NewsSlider/>
-                <div className="sticky top-0 z-10 bg-[#1c1c25]">
+                <div className="sticky top-0 z-30 bg-[#1c1c25]">
                     <FilterSection/>
                 </div>
                 <Channels/>
