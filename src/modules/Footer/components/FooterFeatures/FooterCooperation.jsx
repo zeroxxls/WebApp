f521f6f12fedbe065c5c0327513fa81b1691dadb
zeroxxls <zeroxxhls@gmail.com>
2025-05-07 15:07:34 +0200

    footer fix

--- src/modules/Footer/components/FooterFeatures/FooterCooperation.jsx
+++ src/modules/Footer/components/FooterFeatures/FooterCooperation.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import { EnvelopeIcon, GlobeAltIcon } from "@heroicons/react/24/outline";
 
 export const FooterCooperation =()=>{
+  const CooperationLinks = [
+    {name:'Discord', path:'https://discord.gg/YEPG8UWF'},
+    {name: 'GitHub', path:'https://github.com/zeroxxls?tab=overview&from=2025-05-01&to=2025-05-07'},
+  ]
     return(
         <div>
           <h3 className="text-blue-500 font-bold mb-4 text-lg">Cooperation</h3>
@@ -14,7 +18,16 @@ export const FooterCooperation =()=>{
               <GlobeAltIcon className="w-4 h-4 mr-2" />
               Social:
               <div className="flex ml-3 space-x-2">
-                {['Discord', 'E-Mail', 'Phone', 'GitHub'].map((social) => (
+                {CooperationLinks.map((item)=> (
+                  <a
+                    key={item}
+                    href={item.path}
+                    className="hover:text-blue-300 transition-colors"
+                  >
+                    {item.name}
+                  </a>
+                ))}
+                {['E-Mail', 'Phone',].map((social) => (
                   <a 
                     key={social} 
                     href="#" 
--- src/modules/Footer/components/FooterFeatures/FooterSections.jsx
+++ src/modules/Footer/components/FooterFeatures/FooterSections.jsx
@@ -1,18 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const FooterSections =()=>{
+  const Links = [
+    {name: 'Explore' , path:'/'},
+    {name: 'News' , path:'NewsPage'},
+    {name: 'Shop', path: '/shop' },
+    {name: 'Registration', path: '/RegisterPage' },
+  ]
     return(
         <div>
         <h3 className="text-blue-500 font-bold mb-4 text-lg">Sections</h3>
         <ul className="space-y-2">
-          {['Explore', 'News', 'Shop', 'Registration'].map((item) => (
-            <li key={item}>
-              <a 
-                href="#" 
+          {Links.map((item) => (
+            <li key={item.name}>
+              <Link 
+                to={item.path}
                 className="text-gray-400 hover:text-blue-300 transition-colors"
               >
-                {item}
-              </a>
+                {item.name}
+              </Link>
             </li>
           ))}
         </ul>
